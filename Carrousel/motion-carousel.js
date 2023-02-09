a41545e599818de70a95dc298e0a972018261124
Raúl Patrón <raul.19070050@itsmotul.edu.mx>
2023-02-09 15:19:07 -0600

    carousel3

--- Carrousel/motion-carousel.js
+++ Carrousel/motion-carousel.js
@@ -2,12 +2,33 @@ import {LitElement, html} from 'lit';
 import {styles} from './styles.js';
 
 export class MotionCarousel extends LitElement {
+  static properties = {
+    selected: {type: Number},
+  };
   static styles = styles;
 
+  constructor() {
+    super();
+    this.selected = 0;
+  }
+
+  selectedInternal = 0;
+
+  get maxSelected() {
+    return this.childElementCount - 1;
+  }
+
+  hasValidSelected() {
+    return this.selected >= 0 && this.selected <= this.maxSelected;
+  }
+
   render() {
+    if (this.hasValidSelected()) {
+      this.selectedInternal = this.selected;
+    }
     return html`
       <div class="fit">
-       <slot></slot>
+        <slot></slot>
       </div>
     `;
   }

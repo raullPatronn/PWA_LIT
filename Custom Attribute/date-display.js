4dc689923680d30fcb92c6be4fc1d46b4a3c2390
Raúl Patrón <raul.19070050@itsmotul.edu.mx>
2023-02-09 12:59:50 -0600

    custom 2

--- Custom Attribute/date-display.js
+++ Custom Attribute/date-display.js
@@ -1,14 +1,15 @@
 import {LitElement, html} from 'lit';
-import {dateConverter} from './date-converter.js';
 
 export class DateDisplay extends LitElement {
   static properties = {
-    date: {converter: dateConverter},
+    date: {attribute: false},
+    dateStr: {type: String, attribute: 'date-str'},
   };
 
   constructor() {
     super();
     this.date = new Date();
+    this.dateStr = '';
   }
 
   render() {

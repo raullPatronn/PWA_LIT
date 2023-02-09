e08d35df74535151f20be2650718547b6fdf88b0
Raúl Patrón <raul.19070050@itsmotul.edu.mx>
2023-02-09 10:38:07 -0600

    Intro to Lit 3

--- Intro to lit/my-element.js
+++ Intro to lit/my-element.js
@@ -1,19 +1,25 @@
 import {LitElement, html} from 'lit';
 
-export class MyElement extends LitElement {
+export class NameTag extends LitElement {
   static properties = {
-    message: {},
+    name: {},
   };
 
   constructor() {
     super();
-    this.message = 'Hello again.';
+    this.name = 'Your name here';
   }
 
   render() {
     return html`
-      <p>${this.message}</p>
+      <p>Hello, ${this.name}</p>
+      <input @input=${this.changeName} placeholder="Enter your name">
     `;
   }
+
+  changeName(event) {
+    const input = event.target;
+    this.name = input.value;
+  }
 }
-customElements.define('my-element', MyElement);
+customElements.define('name-tag', NameTag);

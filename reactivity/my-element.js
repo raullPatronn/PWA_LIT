72fe21de71e09323543b6b8e172edc110fea5b3f
Raúl Patrón <raul.19070050@itsmotul.edu.mx>
2023-02-09 12:03:28 -0600

    reactivity 3

--- reactivity/my-element.js
+++ reactivity/my-element.js
@@ -1,29 +1,60 @@
-import {LitElement, html} from 'lit';
-import {map} from 'lit/directives/map.js';
+import {LitElement, html, css} from 'lit';
+import {classMap} from 'lit/directives/class-map.js';
+import {animate} from '@lit-labs/motion';
 
 export class MyElement extends LitElement {
   static properties = {
-    groceries: {},
+    big: {type: Boolean},
+    duration: {type: Number},
+    _renderCount: {state: true},
   };
+
+  static styles = css`
+    .bar {
+      background: red;
+      height: 2em;
+      width: 10vw;
+    }
+
+    .big {
+      width: 50vw;
+    }
+  `;
 
   constructor() {
     super();
-    this.groceries = ['tea', 'milk', 'honey', 'chocolate'];
+    this.big = false;
+    this.duration = 500;
+    this._renderCount = 0;
   }
 
-  removeItem(item) {
-    const indexToRemove = this.groceries.indexOf(item);
-    this.groceries = this.groceries.filter((_, i) => i !== indexToRemove);
+  setDuration(e) {
+    const v = e.target.value;
+    this.duration = Number.parseInt(v);
+  }
+
+  shouldUpdate(changedProperties) {
+    return !(changedProperties.size === 1 && changedProperties.has('duration'));
   }
 
   render() {
+    this._renderCount++;
+    const keyframeOptions = {duration: this.duration};
+
     return html`
-      ${map(
-        this.groceries,
-        (item) =>
-          html`<button @click=${() =>
-            this.removeItem(item)}>x</button>  ${item}<br>`
-      )}
+      <p>
+        <button @click=${() => (this.big = !this.big)}>Animate</button>
+      </p>
+      <p>
+        <label>Speed <select @change=${this.setDuration}>
+          <option value="250" selected>Fast</option>
+          <option value="1500">Slow</option>
+        </select></label>
+        Render count: ${this._renderCount}
+      </p>
+      <p class="bar ${classMap({big: this.big})}" ${animate({
+      keyframeOptions,
+    })}></p>
     `;
   }
 }

039d14e6c47ab32616b52c6b5fb9be1d66b8ade9
Raúl Patrón <raul.19070050@itsmotul.edu.mx>
2023-02-09 08:11:41 -0600

    Intro To Lit

---
+++ Intro to lit/my-element.js
@@ -0,0 +1,10 @@
+import {LitElement, html} from 'lit';
+
+export class MyElement extends LitElement {
+  render() {
+    return html`
+      <p>Hello world! From my-element.</p>
+    `;
+  }
+}
+customElements.define('my-element', MyElement);

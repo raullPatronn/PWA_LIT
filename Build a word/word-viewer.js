3c88a5c44d8e7922f520e1891dd7698262839721
Raúl Patrón <raul.19070050@itsmotul.edu.mx>
2023-02-09 11:52:22 -0600

    build 5

--- Build a word/word-viewer.js
+++ Build a word/word-viewer.js
@@ -12,10 +12,27 @@ class WordViewer extends LitElement {
     this.words = 'initial value';
   }
 
+  intervalTimer;
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.intervalTimer = setInterval(this.tickToNextWord, 1000);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    clearInterval(this.intervalTimer);
+    this.intervalTimer = undefined;
+  }
+
   render() {
     const splitWords = this.words.split('.');
     const word = splitWords[this.idx % splitWords.length];
     return html`<pre>${word}</pre>`;
   }
+
+  tickToNextWord = () => {
+    this.idx += 1;
+  };
 }
 customElements.define('word-viewer', WordViewer);

f751d4ca5df2257af6b015c90a3d68b330bbb129
Raúl Patrón <raul.19070050@itsmotul.edu.mx>
2023-02-09 11:51:31 -0600

    build 4

--- Build a word/word-viewer.js
+++ Build a word/word-viewer.js
@@ -2,16 +2,20 @@ import {html, LitElement} from 'lit';
 
 class WordViewer extends LitElement {
   static properties = {
+    idx: {state: true},
     words: {},
   };
 
   constructor() {
     super();
+    this.idx = 0;
     this.words = 'initial value';
   }
 
   render() {
-    return html`<pre>${this.words}</pre>`;
+    const splitWords = this.words.split('.');
+    const word = splitWords[this.idx % splitWords.length];
+    return html`<pre>${word}</pre>`;
   }
 }
 customElements.define('word-viewer', WordViewer);
